2a59490fe5e0fcfc98a9884bb28116cd1ff13ec6
Nida <147906714+kuwapica@users.noreply.github.com>
2025-10-05 16:40:06 +0700

    Revert "Fitur/add post"

--- app/src/main/java/com/example/lostnfound/data/model/FormStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lostnfound.data.model
-
-sealed class FormStatus {
-    object Idle : FormStatus()
-    object Loading : FormStatus()
-    data class Success(val message: String) : FormStatus()
-    data class Error(val message: String) : FormStatus()
-}
